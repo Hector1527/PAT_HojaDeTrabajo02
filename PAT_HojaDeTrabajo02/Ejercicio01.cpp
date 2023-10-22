3a9e828b3d7b6ae30f5c77a0799aea3beb5a7ef4
Hector1527 <hectorperezgonzalez09@gmail.com>
2023-10-22 15:46:52 -0600

    Ejercicio 1

--- PAT_HojaDeTrabajo02/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio01.cpp
@@ -2,5 +2,29 @@
 
 Node<char>* Ejercicio01::rotateRight(Node<char>* head, int k)
 {
-    return nullptr;
+    if (head == nullptr || k == 0)
+    {
+        return head;
+    }
+
+    Node<char>* tail = head;
+    int nodos = 0;
+    while (tail->next != nullptr)
+    {
+        tail = tail->next;
+        nodos++;
+    }
+
+    Node<char>* Rotate = head;
+    for (int i = 0; i < nodos-k; i++)
+    {
+        Rotate = Rotate->next;
+    }
+
+    Node<char>* result = Rotate;
+    Rotate->next = nullptr;
+    tail->next = head;
+
+
+    return head;
 }
