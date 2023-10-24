d752d3159e36c0dc413870511b52397495f22229
Hector1527 <hectorperezgonzalez09@gmail.com>
2023-10-23 19:50:02 -0600

    Ejercicio 4

--- PAT_HojaDeTrabajo02/Ejercicio04.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio04.cpp
@@ -1,32 +1,175 @@
 #include "Ejercicio04.h"
+#include <iostream>
 
 FrontMiddleBackQueue::FrontMiddleBackQueue()
 {
 }
 
+
+
 void FrontMiddleBackQueue::pushFront(int value)
 {
+	if (head == nullptr)
+	{
+		head = tail = new Nodo(value);
+	}
+	else
+	{
+		Nodo* temp = new Nodo(value);
+		temp->next = head;
+		head = temp;
+	}
 }
 
 void FrontMiddleBackQueue::pushMiddle(int value)
 {
+	Nodo* temp = new Nodo(value);
+
+	if (head == nullptr)
+	{
+		head = tail = temp;
+	}
+	else
+	{
+		Nodo* corto = head;
+		Nodo* largo = head;
+		Nodo* prev = nullptr;
+
+		while (largo != nullptr && largo->next != nullptr)
+		{
+			largo = largo->next->next;
+			prev = corto;
+			corto = corto->next;
+		}
+
+		if (prev == nullptr)
+		{
+			temp->next = head;
+			head = temp;
+		}
+		else
+		{
+			temp->next = corto;
+			prev->next = temp;
+		}
+	}
 }
 
 void FrontMiddleBackQueue::pushBack(int value)
 {
+
+	if (head == nullptr)
+	{
+		head = tail = new Nodo(value);
+	}
+	else
+	{
+		Nodo* temp = new Nodo(value);
+		tail->next = temp;
+		tail = temp;
+	}
 }
 
 int FrontMiddleBackQueue::popFront()
 {
-	return 0;
+	if (head == nullptr)
+		return -1;
+
+	Nodo* temp = head;
+	int valor = temp->num;
+	head = head->next;
+
+	delete temp;
+	return valor;
 }
 
 int FrontMiddleBackQueue::popMiddle()
 {
-	return 0;
+	if (head == nullptr)
+		return -1;
+
+	Nodo* prev = nullptr;
+	Nodo* corto = head;
+	Nodo* largo = head;
+	Nodo* prevprev = nullptr;
+	int cantidad = 0;
+
+	while (largo != nullptr && largo->next != nullptr)
+	{
+		prevprev = prev;
+		largo = largo->next->next;
+		prev = corto;
+		corto = corto->next;
+		cantidad += 2;
+	}
+
+	if (largo != nullptr)
+		cantidad += 1;
+
+	if (cantidad == 1)
+	{
+		int valor = largo->num;
+		head = head->next;
+		return valor;
+	}
+
+	if (cantidad == 2)
+	{
+		int valor = head->num;
+		Nodo* temp = head;
+		head = head->next;
+		delete temp;
+		return valor;
+	}
+	else {
+		if (prev == nullptr)
+		{
+			head = corto->next;
+		}
+		else if (cantidad % 2 == 0)
+		{
+			int valor = prev->num;
+			prevprev->next = prev->next;
+			delete prev;
+			return valor;
+		}
+		else
+		{
+			int valor = corto->num;
+			prev->next = corto->next;
+			delete corto;
+			return valor;
+		}
+	}
 }
 
 int FrontMiddleBackQueue::popBack()
 {
-	return 0;
+
+	if (head == nullptr)
+		return -1;
+
+	Nodo* temp = head;
+	Nodo* prev = nullptr;
+
+	while (temp->next != nullptr)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+	int valor = temp->num;
+
+	if (prev != nullptr)
+	{
+		prev->next = nullptr;
+		tail = prev;
+	}
+	else
+	{
+		head = nullptr;
+		tail = nullptr;
+	}
+
+	delete temp;
+	return valor;
 }
--- PAT_HojaDeTrabajo02/Ejercicio04.h
+++ PAT_HojaDeTrabajo02/Ejercicio04.h
@@ -2,6 +2,15 @@
 class FrontMiddleBackQueue
 {
 public:
+	struct Nodo
+	{
+		int num;
+		Nodo* next = nullptr;
+		Nodo(int value) : num(value) {}
+	};
+	Nodo* head = nullptr;
+	Nodo* tail = nullptr;
+
 	FrontMiddleBackQueue();
 
 	void pushFront(int value);
